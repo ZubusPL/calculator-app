74d264a42f993131284dd3b62d4783f8ed9f77a0
ZubusPL <zubus.official@gmail.com>
2018-05-30 04:00:17 +0200

    Javascript update, application mechanism

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,60 @@
 window.onload = function() {
-	console.log('function onload executed');
+	var
+		keys = [],
+		buttons = {},
+		$interface = null,
+		contentString;
+
+	$interface = document.getElementById('interface');
+	keys = $interface.getElementsByClassName('calculator__button');
+	contentString = document.getElementById('content-string').innerHTML;
+
+	buttons.clear = document.getElementById('clear');
+	buttons.settings = document.getElementById('settings');
+
+	for (var i = 0; i < keys.length; i++) {
+		keys[i].addEventListener('click', onClickInterfaceButton, false);
+	}
+
+	buttons.clear.addEventListener('click', onClickClearButton, false);
+	buttons.settings.addEventListener('click', onClickSettingsButton, false);
+
+	function onClickInterfaceButton(e) {
+		e.preventDefault();
+
+		var _data = {
+			element: e.target.getAttribute('data-element'),
+			action: e.target.getAttribute('data-action'),
+			value: e.target.getAttribute('value')
+		};
+
+		if (_data.element !== null && _data.element === 'key') {
+			updateView(_data.value, 'none');
+		} else if(_data.element === null && _data.value === null && _data.action !== null) {
+			updateView('none', _data.action);
+		}
+	}
+
+	function onClickClearButton(e) {
+		e.preventDefault();
+
+		if (buttons.clear !== 'undefined') {
+			console.log('yup!');
+		}
+	}
+
+	function onClickSettingsButton(e) {
+		e.preventDefault();
+
+		if (buttons.settings !== 'undefined') {
+			console.log('yeah!');
+		}
+	}
+
+	function updateView(value, action) {
+		contentString = '';
+		contentString = action + ' ' + value + '<br/>';
+		console.log(value, action);
+		console.log(contentString);
+	}
 }
